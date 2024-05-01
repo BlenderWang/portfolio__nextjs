7a4bde7a56da97d960011efb2aa7a41591f7ee3c
Shirley Wang <shirley@devup.solutions>
2024-05-01 19:50:30 +0200

    fix ts error

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,15 @@
 // import { forwardRef } from 'react'
-import clsx from 'clsx'
+import clsx from "clsx";
 
-export const Container = ({ className, children, ...props }) => {
+interface Props {
+  className: string;
+  children: React.ReactNode;
+}
+
+export const Container = ({ className, children, ...props }: Props) => {
   return (
-    <div className={clsx('px-8 xl:px-0', className)} {...props}>
+    <div className={clsx("px-8 xl:px-0", className)} {...props}>
       <div className="mx-auto w-full max-w-7xl lg:px-8">{children}</div>
     </div>
-  )
-}
+  );
+};
